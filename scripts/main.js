91bf00c592e3a314343e43af1306df6914f018c5
sachindhoundiyal02 <84776765+sachindhoundiyal02@users.noreply.github.com>
2025-02-20 15:43:30 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector(".head-content");
+myHeading.textContent = "Improve your front-end skills by building projects";
+
+const myPara = document.querySelector(".paragraph-content");
+myPara.textContent = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level";
+
+
+// const myBtn = document.querySelector(".bottom-btn");
+// myBtn.textContent = "OK";
